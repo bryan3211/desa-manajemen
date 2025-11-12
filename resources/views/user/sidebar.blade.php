acd7d41890d1fe904cc517dae57eea26fa49fff4
Badar5G <bryanandry906@gmail.com>
2025-11-12 09:00:05 +0700

    update fitur baru

--- resources/views/user/sidebar.blade.php
+++ resources/views/user/sidebar.blade.php
@@ -1,5 +1,2 @@
-<li class="pc-item"><a href="{{ route('user.biodata') }}" class="pc-link"><span class="pc-micon"><i
-                class="ti ti-user"></i></span><span class="pc-mtext">Isi
-            Biodata</span></a></li>
 <li class="pc-item"><a href="{{ route('user.pengaduan.index') }}" class="pc-link"><span class="pc-micon"><i
                 class="ti ti-alert-circle"></i></span><span class="pc-mtext">Pengaduan Surat</span></a></li>
